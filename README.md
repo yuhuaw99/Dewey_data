cda01883fc1f36accf49b469ed36a34d7f79f033
Yuhua Wang <yuhua_11@live.com>
2025-02-05 11:55:13 -0500

    update on montly mobility pattern data_aggregation

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ Use 01_download_data_public.ipynb to download needed data
 
 reference: https://docs.deweydata.io/docs/api-access
 
-## Data preprocessing
+## Monthly mobility pattern 
 Use 02_data_preprocess.ipynb as reference and use 02_data_preprocess to enable parallel processing to increase speed
+
+Use 03_data_aggregation to aggregate seperate file into on big file for each month
+
 
 
---
+++ monthly_pattern/03_data_aggregation.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import concurrent.futures
+import os
+
+def read_csv_file(file_path):
+    dtype_dict = {
+        'category':'category'
+    }
+    return pd.read_csv(file_path, dtype=dtype_dict)
+
+def parallel_read_and_concat(file_paths):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        dfs = list(executor.map(read_csv_file, file_paths))
+
+    combined_df = pd.concat(dfs, ignore_index=True)
+    return combined_df
+
+
+if __name__ == '__main__':
+    files = os.listdir('2024_01_05_preprocessed')
+    months = [str(x).zfill(2) for x in range(1,6)]
+    for month in months:
+        file_paths = [f'2024_01_05_preprocessed/{file}' for file in files if f'{month}-01' in file]
+        # print(file_paths[0:10])
+        # print('------------------')
+        # print(file_paths)
+        combined_df = parallel_read_and_concat(file_paths)
+        combined_df.to_csv('2024_01_05_monthly/'+'monthly_pattern_'+month+'.csv')
+        del combined_df
